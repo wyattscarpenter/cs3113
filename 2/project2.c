66c868e67aa49690a62ad56b4823c03b66d1378d
wyattscarpenter <wyattscarpenter@gmail.com>
2018-10-23 16:09:00 -0500

    remove some error checking
    might have been messing things up

--- 2/project2.c
+++ 2/project2.c
@@ -176,18 +176,14 @@ int mimic(const char * src, const char * dst, int recursive){
     ret = copy(src, srcindst);
   } else if (srcdir && pdstdir){
     if(recursive){
-      if( (ret = mkdir(dst, ALL_PERM)) ){
-	do{
-	  char * nextsrc = slash(src,firstinsrc->d_name);
-	  char * nextdst = slash(dst,firstinsrc->d_name);
-	  mimic(nextsrc, nextdst, recursive);
-	  free(nextsrc);
-	  free(nextdst);
-	}while( (firstinsrc = getent(srcdir)) );
-      } else {
-	error("couldn't mkdir in mimic");
-	return ret;
-      }
+      ret = mkdir(dst, ALL_PERM);
+      do{
+	char * nextsrc = slash(src,firstinsrc->d_name);
+	char * nextdst = slash(dst,firstinsrc->d_name);
+	mimic(nextsrc, nextdst, recursive);
+	free(nextsrc);
+	free(nextdst);
+      }while( (firstinsrc = getent(srcdir)) );
     } else if (!firstinsrc) {
       ret = mkdir(dst, ALL_PERM);
     } else {
