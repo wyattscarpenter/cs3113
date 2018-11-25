8184735cc516c82aa6b0bd60826215096ca24978
wyattscarpenter <wyattscarpenter@gmail.com>
2018-11-25 23:49:12 +0000

    modify code

--- hw4/thread_incr_psem.c
+++ hw4/thread_incr_psem.c
@@ -1,30 +1,12 @@
-/*************************************************************************\
-*                  Copyright (C) Michael Kerrisk, 2018.                   *
-*                                                                         *
-* This program is free software. You may use, modify, and redistribute it *
-* under the terms of the GNU General Public License as published by the   *
-* Free Software Foundation, either version 3 or (at your option) any      *
-* later version. This program is distributed without any warranty.  See   *
-* the file COPYING.gpl-v3 for details.                                    *
-\*************************************************************************/
-
-/* Listing 53-6 */
-
-/* thread_incr_psem.c
-
-   Use a POSIX unnamed semaphore to synchronize access by two threads to
-   a global variable.
-
-   See also thread_incr.c and thread_incr_mutex.c.
-*/
 #include <semaphore.h>
 #include <pthread.h>
-#include "tlpi_hdr.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-static int glob = 0;
+static int global = 0;
 static sem_t sem;
 
-static void *                   /* Loop 'arg' times incrementing 'glob' */
+static void *                   /* Loop 'arg' times incrementing 'global' */
 threadFunc(void *arg)
 {
     int loops = *((int *) arg);
@@ -32,50 +14,45 @@ threadFunc(void *arg)
 
     for (j = 0; j < loops; j++) {
         if (sem_wait(&sem) == -1)
-            errExit("sem_wait");
+	  exit(-1);
 
-        loc = glob;
-        loc++;
-        glob = loc;
+        global++;
 
         if (sem_post(&sem) == -1)
-            errExit("sem_post");
+	  exit(-1);
     }
 
     return NULL;
 }
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     pthread_t t1, t2;
-    int loops, s;
+    int loops, threads, s;
 
-    loops = (argc > 1) ? getInt(argv[1], GN_GT_0, "num-loops") : 10000000;
+    loops = atol(argv[1]);
 
     /* Initialize a semaphore with the value 1 */
 
     if (sem_init(&sem, 0, 1) == -1)
-        errExit("sem_init");
+      exit(-1);
 
-    /* Create two threads that increment 'glob' */
+    /* Create two threads that increment 'global' */
 
     s = pthread_create(&t1, NULL, threadFunc, &loops);
     if (s != 0)
-        errExitEN(s, "pthread_create");
+      exit(-1);
     s = pthread_create(&t2, NULL, threadFunc, &loops);
     if (s != 0)
-        errExitEN(s, "pthread_create");
-
-    /* Wait for threads to terminate */
+      exit(-1);
+      /* Wait for threads to terminate */
 
     s = pthread_join(t1, NULL);
     if (s != 0)
-        errExitEN(s, "pthread_join");
+      exit(-1);
     s = pthread_join(t2, NULL);
     if (s != 0)
-        errExitEN(s, "pthread_join");
-
-    printf("glob = %d\n", glob);
+      exit(-1);
+    printf("global = %d\n", global);
     exit(EXIT_SUCCESS);
 }
