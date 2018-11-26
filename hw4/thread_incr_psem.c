dd054bfaf660c092495b0082fe32a2940a39625a
wyattscarpenter <wyattscarpenter@gmail.com>
2018-11-26 00:12:46 +0000

    modification

--- hw4/thread_incr_psem.c
+++ hw4/thread_incr_psem.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-static int global = 0;
+static unsigned int global = 0;
 static sem_t sem;
 
 static void *                   /* Loop 'arg' times incrementing 'global' */
@@ -27,32 +27,30 @@ threadFunc(void *arg)
 
 int main(int argc, char *argv[])
 {
-    pthread_t t1, t2;
-    int loops, threads, s;
+  int MAX_THREADS = 255;
+    pthread_t t[MAX_THREADS];
+    long loops, threads, s;
 
     loops = atol(argv[1]);
-
+    threads = atol(argv[2]);
+    
     /* Initialize a semaphore with the value 1 */
 
     if (sem_init(&sem, 0, 1) == -1)
       exit(-1);
 
-    /* Create two threads that increment 'global' */
-
-    s = pthread_create(&t1, NULL, threadFunc, &loops);
-    if (s != 0)
-      exit(-1);
-    s = pthread_create(&t2, NULL, threadFunc, &loops);
-    if (s != 0)
-      exit(-1);
+    /* Create n threads that increment 'global' */
+    for(int i = 0; i < threads; i++){
+      s = pthread_create(&t[i], NULL, threadFunc, &loops);
+      if (s != 0)
+	exit(-1);
+    }
       /* Wait for threads to terminate */
-
-    s = pthread_join(t1, NULL);
+    for(int i = 0; i < threads; i++){
+    s = pthread_join(t[i], NULL);
     if (s != 0)
       exit(-1);
-    s = pthread_join(t2, NULL);
-    if (s != 0)
-      exit(-1);
+    }
     printf("global = %d\n", global);
     exit(EXIT_SUCCESS);
 }
